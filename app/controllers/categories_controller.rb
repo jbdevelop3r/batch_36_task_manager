370e7d33bf68a83bbe1518a0e849e8013971d5b7
jbdevelop3r <justinebanogon14@gmail.com>
2024-10-26 09:53:13 +0800

    modifed render key

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,54 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+
   def index
     @categories = Category.all
+    @tasks_today = Task.where(due_date: Date.today)
   end
 
   def show
-    @category = Category.find(params[:id])
     @category_tasks = @category.tasks
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category, notice: 'Category was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category, notice: 'Category was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @category.destroy
+      redirect_to categories_url, notice: 'Category was successfully destroyed.'
+    else
+      redirect_to categories_url, alert: 'Category could not be destroyed.'
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:category_name)
   end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,55 @@
 class TasksController < ApplicationController
+  before_action :set_category
+  before_action :set_task, only: %i[show edit update destroy]
+
   def index
-    @tasks = Tasks.all
+    @tasks = @category.tasks
   end
 
   def show
-    @task = Task.find(params[:id])
   end
+
+  def new 
+    @task = @category.tasks.build
+  end
+
+  def create 
+    @task = @category.tasks.build(task_params)
+
+    if @task.save  
+      redirect_to category_task_path(@category, @task), notice: 'Task was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to category_task_path(@category, @task), notice: 'Task was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to category_tasks_path(@category), notice: 'Task was successfully deleted.'
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_task
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:task_name, :description, :due_date)
+  end  
 end
